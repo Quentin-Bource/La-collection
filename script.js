8d4579d3b564d11cf360ad981fb620f84cd9f909
Quentin <quentin01.bource@gmail.com>
2022-12-13 21:35:02 +0100

    collection

---
+++ script.js
@@ -0,0 +1,70 @@
+const arr = ["Ratchet & Clank 3", "Burnout 3 Takedowm", "Sly Cooper 3",
+ "Dragon ball Z budokai 3", "Dofus", "Assassin's Creed Brotherhood",
+  "The Witcher 3", "Borderlands 2" , "Dragon Quest XI", "Tekken 5", "God of War 2018","Spider-man 2" ]
+
+const collection = [
+    // VG1
+    {
+        name: "Ratchet & Clank 3",
+        dev:"Insomniacs Games", 
+        sortie: "2004",
+        img: "/assets/img/ratchet&clank3.jpg",
+        genre: [
+            "Plates-formes", "Action", "Tir"
+        ],
+        plates_formes: ["PlayStation 3", "PlayStation 2", "PlayStation Vita"],
+    },
+
+    //VG2
+
+    {
+        name: "Burnout 3 Takedown",
+        dev: "Criterion Software",
+        sortie: "2004",
+        img: "/assets/img/burnout.jpg",
+        genre: ["Course","Action"],
+        plates_formes: ["Playstation 2","Xbox", "Xbox360"],
+    },
+
+    // VG3 
+    {
+        name: "Sly 3",
+        dev: "Sucker Punch Productions",
+        sortie: "2005",
+        img: "/assets/img/sly.jpg",
+        genre: ["Plates-formes","Infiltration", "Action"],
+        plates_formes: ["Playstation 2"],
+
+    },
+
+    //VG4
+    {
+        name: "Dragon Ball Z Budokai 3",
+        dev: "Dimps",
+        sortie: "2004",
+        img: "/assets/img/dbz3.jpg",
+        genre: ["Combat","Action"],
+        plates_formes: ["Playstation 2"],
+    },
+
+    //VG5
+    {
+    name: "Dofus",
+    dev: "Ankama",
+    sortie: "2004",
+    img: "/assets/img/dofus.png",
+    genre: ["MMORPG"],
+    plates_formes: ["PC"],
+    },
+
+    //VG6
+    {
+        name: "Assassin's Creed Brotherhood",
+        dev: "Ubisoft Montréal",
+        sortie: "2010",
+        img: "/assets/img/acb.png",
+        genre: ["Aventure","Action","RPG"],
+        plates_formes: ["Playstation 3",, "Xbox360","PC","Playstation 4","Xbox One", "Switch",],
+    }
+
+]
